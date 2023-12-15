d73447e341ea716851d316fac08c008ec590aa42
nzyoka10 <mnzyokaeric@gmail.com>
2023-12-15 17:45:06 +0300

    accessing php

---
+++ customer_order/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bob's Auto Parts - Order Form</title>
+    <link rel="stylesheet" href="./styles.css">
+</head>
+<body>
+
+    <section>
+        <h1>customer order</h1>
+        <form action="./process_order.php" method="post">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Item</th>
+                        <th>Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Tires</td>
+                        <td><input type="number" name="tireqty" value="0" min="0"></td>
+                    </tr>
+                    <tr>
+                        <td>Oil</td>
+                        <td><input type="number" name="oilQuantity" value="0" min="0"></td>
+                    </tr>
+                    <tr>
+                        <td>Spark Plugs</td>
+                        <td><input type="number" name="sparkPlugsQuantity" value="0" min="0"></td>
+                    </tr>
+                </tbody>
+            </table>
+            <input type="submit" value="Submit Order">
+        </form>
+    </section>
+
+</body>
+</html>
---
+++ customer_order/process_order.php
@@ -0,0 +1,8 @@
+<?php
+
+    # process the customers order table
+    
+
+
+
+?>
