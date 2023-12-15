00f8245aa81506bfdd433a2edd9d3c0382d1505e
nzyoka10 <mnzyokaeric@gmail.com>
2023-12-15 19:15:52 +0300

    accesing form variables

--- customer_order/process_order.php
+++ customer_order/process_order.php
@@ -5,16 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bob's Auto Parts - Order Results</title>
     <link rel="stylesheet"href="./styles.css">
+    <style>
+        #main{
+            border: 3px;
+            padding: 18px;
+            margin: 28px;
+        }
+    </style>
 </head>
 <body>
     
-    <h1>bob's auto parts</h1>
-    <h2>Order Results</h2>
+    <div id="main">
 
-    <?php 
+        <h1>bob's auto parts</h1>
+        <h2>Order Results</h2>
+
+
+        <?php 
         # process the customers order table here....
         echo "<p>Order processed at ".date('H:i, j : s F Y')." </p>";
+
+        # variable names decralation
+        $tireqty = $_POST['tireqty'];
+        $oilqty = $_POST['oilqty'];
+        $sparkqty = $_POST['sparkqty'];
+
+        # output Tire value
+        echo $tireqty.' tires <br/>';
+
     ?>
+
+    </div>
+
+   
 
     
 </body>
