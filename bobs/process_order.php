2dc72f311be4a131d0cf92f19e4942ea56e1976a
nzyoka10 <mnzyokaeric@gmail.com>
2023-12-19 16:54:34 +0300

    customers page

--- bobs/process_order.php
+++ bobs/process_order.php
@@ -25,7 +25,7 @@
      */
 
         date_default_timezone_set('Africa/Nairobi');
-        $date = date('d-M-Y, H :i');
+        $date = date('d-M-Y, H:i');
         # echo $date;
 
         echo '<p style="color: green;">Order processed at, '.$date. '</p>';
@@ -36,20 +36,34 @@
         $sparkqty = $_POST['sparkqty'];
 
         echo '<p>Your order is as follows: </p>';
-        echo htmlspecialchars($tireqty).' <b>Tires</b><br/>';
-        echo htmlspecialchars($oilqty).' <b>Bottles of oil</b><br/>';
-        echo htmlspecialchars($sparkqty).' <b>Spark plugs</b>';
-
+        
         
         $totalqty = 0;
-        $totalqty = $tireqty + $oilqty + $sparkqty;
+        $totalqty = intval($tireqty) + intval($oilqty) + intval($sparkqty);
 
+        # capture orders
         if ($totalqty == 0) 
         {
             echo '<p style="color: red;">';
             echo 'You did not order anything on the previous page!';
             echo '</p>';
         }
+        else
+        {
+            if($tireqty > 0)
+                #echo "Tires ordered: {$tireqty} set(s).<br>";
+                echo htmlspecialchars($tireqty).' <b>Tires</b><br/>';
+            if ($oilqty)
+                # echo "Oils ordered: {$oilqty}.<br>";
+                echo htmlspecialchars($oilqty).' <b>Bottles of oil</b><br/>';
+            if ($sparkqty)
+                # echo "Spares ordered: {$sparkqty}.<br><br>";
+                echo htmlspecialchars($sparkqty).' <b>Spark plugs</b>';
+        }
+      
+        
+        
+
 
         echo "<p>Items Ordered: ".$totalqty."<br/>";           
 
@@ -61,9 +75,14 @@
             define('SPARKPRICE', 4);
 
             # $totalqty = 0;
+            $total_amount = floatval($tireqty) * TIREPRICE 
+                                    + floatval($oilqty) * OILPRICCE 
+                                    + floatval($sparkqty) * SPARKPRICE;
+/**
             $total_amount = $tireqty * TIREPRICE
                         + $oilqty * OILPRICCE
                         + $sparkqty * SPARKPRICE;
+        */
 
             echo "Subtotal: Kes:. ".number_format($total_amount, 2)."<br/>";
 
